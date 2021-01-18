2a9c2d8b358a26fff4d955cac93dbf004d24fdac
Chi <rectaflex@gmail.com>
2021-01-17 20:59:46 -0500

    deleting more legacy files

--- __init__.py
+++ __init__.py
@@ -2,15 +2,15 @@
 
 #__all__ = ['lcmetric.lightcone','lcmetric.utils']
 
-import lcmetric.lightcone
-import lcmetric.lightcone_it
-import lcmetric.lightcone_it_mg
-import lcmetric.lightcone_it_lm
+#import lcmetric.lightcone
+#import lcmetric.lightcone_it
+#import lcmetric.lightcone_it_mg
+#import lcmetric.lightcone_it_lm
 import lcmetric.lightcone_it_mg_lm
-import lcmetric.lightcone_it_lm_hp
-import lcmetric.lightcone_pysh
-import lcmetric.lcmetric.lightcone_it_C
+#import lcmetric.lightcone_it_lm_hp
+#import lcmetric.lightcone_pysh
+#import lcmetric.lcmetric.lightcone_it_C
 import lcmetric.utils
-import lcmetric.lcmetric.utils_C
+#import lcmetric.lcmetric.utils_C
 
 
--- setup.py
+++
@@ -1,8 +0,0 @@
-import pyximport
-pyximport.install(reload_support=True)
-
-from distutils.core import setup
-from Cython.Build import cythonize
-
-setup(name='utils_C', ext_modules=cythonize('utils_C.pyx'),)
-setup(name='lightcone_it_C', ext_modules=cythonize('lightcone_it_C.pyx', language="c++"),)
