8f48e08378ea27dc92c7489920ed67c2a1f48e13
rectaflex <rectaflex@gmail.com>
2021-01-29 18:48:00 -0500

    fsdlj

--- lightcone_CIC.pyx
+++ lightcone_CIC.pyx
@@ -5,11 +5,13 @@ cimport clightcone_CIC as lc_CIC
 
 import numpy as npy
 
-cpdef deposit(double [:,::1] particles, double [:] origin, double [:,::1] delta, \
-              double density_count,                                          \
-              double [:,::1] vw, double max_r, int NR, int NSIDE):
+
+
+cpdef deposit(real_t [:,::1] particles, real_t [:] origin, real_t [:,::1] delta, \
+              real_t density_count,                                          \
+              real_t [:,::1] vw, real_t max_r, real_t min_r, idx_t NR, idx_t NSIDE, idx_t vx_is_weight = 0):
     nparticles = particles.shape[0]
     lc_CIC.CIC_deposit(&particles[0,0], &origin[0], &delta[0,0], &vw[0,0],   \
                        nparticles, density_count,                            \
-                       max_r, NR, NSIDE)
+                       max_r, min_r, NR, NSIDE, vx_is_weight)
     
