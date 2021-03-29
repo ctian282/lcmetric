3446ac9a0811dda3fc9c6976e04518d83fa57e8e
Chi <rectaflex@gmail.com>
2021-03-29 19:47:18 -0400

    adding nearest neighbor deposit

--- cython/c++/lightcone_CIC.h
+++ cython/c++/lightcone_CIC.h
@@ -274,3 +274,88 @@ void CIC_deposit_with_wgt(
         }
     }
 }
+
+
+// nearest particle deposit
+template<typename T>
+void NP_deposit(T *particles, double *origin, T *delta,
+                 T *vw, T *counts,
+                 idx_t nparticles, double count_density,
+                 double max_r, double min_r, int NR, int NSIDE, int vx_is_weight)
+{
+    // Healpix instance
+    T_Healpix_Base<int> * HP = new T_Healpix_Base<int>
+        ( NSIDE, Healpix_Ordering_Scheme::RING, SET_NSIDE );
+
+    double dr = (max_r - min_r) / NR;
+
+    int NPIX = 12*NSIDE*NSIDE;
+
+#pragma omp parallel for
+    for(idx_t p = 0; p < nparticles; p++)
+    {
+        double x = particles[p*6 + 0] - origin[0];
+        double y = particles[p*6 + 1] - origin[1];
+        double z = particles[p*6 + 2] - origin[2];
+        double vx = particles[p*6 + 3];
+        double vy = particles[p*6 + 4];
+        double vz = particles[p*6 + 5];
+
+        double r = std::sqrt(x*x + y*y + z*z);
+        // get chi, theta, phi
+        double theta = std::acos(z/r);
+        if(theta < -0.1) throw(-1);
+        if(theta >= M_PI) theta = M_PI - 1.0e-6; // correct for floating point error
+        double phi = std::atan2(y, x);
+        if(phi <= 0.0) { phi = 2.0*M_PI + phi; } // atan2 in range (-pi,pi) -> (0,2pi)
+        if(phi >= 2.0*M_PI) phi = 2.0*M_PI - 1.0e-6;
+
+        double vr = (vx * x + vy * y + vz * z) / r;
+
+        if(vx_is_weight > 0)
+        {
+            vr = vx;
+        }
+
+        int ic = CHI2CHIBIN(r-min_r, dr);
+        int icp = ic+1;
+
+        if(icp >= NR + 2 || ic < -1) continue;
+
+        double chi_min = r - dr/2.0;
+        double chi_max = r + dr/2.0;
+        double expected_counts = count_density * 4.0/3.0*M_PI*(
+            std::pow(chi_max,3) - std::pow(chi_min,3) ) / ((double)      \
+                                                           NPIX);
+
+        double dc = (r-min_r) / dr - (double)ic;
+
+        double tc = 1.0 - dc;
+
+        pointing ptg = pointing(theta, phi);
+        //auto pix = fix_arr<int, 4>();
+        //auto wgt = fix_arr<double, 4>();
+        //HP->get_interpol(ptg, pix, wgt);
+        auto pix = HP->ang2pix(ptg);
+
+        if(dc <= 0.5 && ic >= 0){
+#pragma omp atomic
+            delta[IDX(ic, pix, NPIX)] += 1.0/expected_counts;
+#pragma omp atomic
+            counts[IDX(ic, pix, NPIX)] += 1;
+#pragma omp atomic
+            vw[IDX(ic, pix, NPIX)] += vr;
+        }
+        else if(dc > 0.5){
+#pragma omp atomic
+            delta[IDX(icp, pix, NPIX)] += 1.0/expected_counts;
+#pragma omp atomic
+            counts[IDX(icp, pix, NPIX)] += 1;
+#pragma omp atomic
+            vw[IDX(icp, pix, NPIX)] += vr;
+
+        }
+
+    }
+
+}
